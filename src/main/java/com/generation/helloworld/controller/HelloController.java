48a3f2f77d2ba24a153af596815726f95984daa9
Joyce Caroline Rodrigues <carolinerodriguesjoyce@gmail.com>
2023-04-12 23:18:29 -0300

    hello

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -15,6 +15,22 @@ public class HelloController {
 	
 	@GetMapping("/nome")
 	public String meuNome() {
-		return "<b>Joyce</b>";
+		return "<b>Hello, Joyce</b>";
 	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<br>Mentalidade de Crescimento\" + \"<br>Persistência\" + \"<br>Responsabilidade Pessoal\"\r\n"
+				+ "				+ \"<br>Orientação ao Futuro\" + \"<br>Comunicação\" + \"<br>Orientação ao Detalhe\" + \"<br>Proatividade\"\r\n"
+				+ "				+ \"<br>Trabalho em Equipe\" + \"<br>Dar e Receber\" + \"<br>Gestão do Tempo<br>";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<b>Como objetivo essa semana tenho a proatividade e gestão de tempo<br /><br />"
+				+ "<b>A minha proatividade essa não está muito bem trabalhada, por falta de confiança"
+				 + "<b>E a minha gestão de tempo está ruim tambem, porque acabei postergando algumas atividades por pura preguiça\"\\r\\n"
+				  + "<b>Por isso necessito intensificar minhas ações com essa habilidade e mentalidade<br>";
+	}
+
 }
